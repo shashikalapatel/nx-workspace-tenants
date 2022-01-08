8ff4c2626ab981c999b106baf40ab1d4d2e7fb5d
shashikala.patel <shashikala.patel@techment.com>
2022-01-08 11:51:39 +0530

    [ADD]- nx-workspace-tenants

---
+++ apps/tenant1/src/app/app.js
@@ -0,0 +1,77 @@
+import styles from './app.module.scss';
+
+import { useDispatch } from 'react-redux';
+import { Route, Link } from 'react-router-dom';
+import { loginUser, logoutUser, resetAuth, signupUser, forgotPassword } from '@shashikalapatel/test-redux-library-v8';
+
+export function App() {
+  const dispatch = useDispatch();
+
+  function loginApi() {
+    dispatch(loginUser("test", "test"));
+  }
+
+  function logoutApi() {
+    dispatch(logoutUser());
+  }
+
+  function signupUserApi() {
+    dispatch(signupUser("test", "test@gmail.com", "test"));
+  }
+
+  function resetAuthApi() {
+    dispatch(resetAuth());
+  }
+
+  function forgotPasswordApi() {
+    dispatch(forgotPassword("test"));
+  }
+
+  return (
+    <>
+    <h3>Tenant1</h3>
+
+      <h4>Redux library Testing</h4>
+      <button onClick={loginApi}>login</button>
+      <button onClick={logoutApi}>logout</button>      
+      <button onClick={signupUserApi}>signup user</button>
+
+      <button onClick={resetAuthApi}>reset auth</button>
+      <button onClick={forgotPasswordApi}>forgot password</button>
+
+      <h4>Routes</h4>
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Route
+        path="/"
+        exact
+        render={() => (
+          <div>
+            This is the generated root route.{' '}
+            <Link to="/page-2">Click here for page 2.</Link>
+          </div>
+        )}
+      />
+      <Route
+        path="/page-2"
+        exact
+        render={() => (
+          <div>
+            <Link to="/">Click here to go back to root page.</Link>
+          </div>
+        )}
+      />
+      {/* END: routes */}
+    </>
+  );
+}
+
+export default App;
---
+++ apps/tenant2/src/main.js
@@ -0,0 +1,18 @@
+import '@babel/polyfill';
+
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './app/app';
+import { Provider } from 'react-redux';
+import { configureStore } from '@shashikalapatel/test-redux-library-v9';
+
+ReactDOM.render(
+  <Provider store={configureStore({})}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
